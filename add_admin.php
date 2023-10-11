2398cfb5a56b96cf24790fb232417cbd2d0059b3
varnac27 <62003779+varnac27@users.noreply.github.com>
2023-10-11 14:47:32 +0530

    Update add_admin.php

--- add_admin.php
+++ add_admin.php
@@ -1,8 +1,8 @@
 <?php
-	$connection = mysqli_connect("localhost","root","");
-	$db = mysqli_select_db($connection,"sms");
-	$query = "insert into login values('$_POST[name]','$_POST[email]','$_POST[password]')";
-	$query_run = mysqli_query($connection,$query);
+	$connection=mysqli_connect("localhost","root","");
+	$db=mysqli_select_db($connection,"sms");
+	$query="insert into login values('$_POST[name]','$_POST[email]','$_POST[password]')";
+	$query_run=mysqli_query($connection,$query);
 ?>
 <script type="text/javascript">
 	alert("Admin added successfully.");
